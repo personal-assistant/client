d90760903bf236d468b6bf3f0c0f6b542e99dae7
eltim12 <eltim2000@gmail.com>
2019-05-17 21:13:58 +0700

    final touch up for login-register

--- containers/Register.js
+++ containers/Register.js
@@ -1,91 +1,161 @@
 import React, { Component } from 'react';
+import axios from '../serverAPI/backendServer'
+import { Permissions, Notifications } from 'expo';
 import { Constants } from 'expo'
 import {
-  StyleSheet,
-  View,
-  KeyboardAvoidingView
+    StyleSheet,
+    View,
+    KeyboardAvoidingView
 } from "react-native";
 import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Footer,
-  FooterTab,
-  Button,
-  Left,
-  Right,
-  Body,
-  Icon,
-  Text,
-  Label,
-  Form,
-  Input,
-  Item,
-  Thumbnail
+    Container,
+    Header,
+    Title,
+    Content,
+    Footer,
+    FooterTab,
+    Button,
+    Left,
+    Right,
+    Body,
+    Icon,
+    Text,
+    Label,
+    Form,
+    Input,
+    Item,
+    Thumbnail
 } from 'native-base';
 
 
 export default class AnatomyExample extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: ''
+    }
 
-  render() {
-    return (
-      <KeyboardAvoidingView 
-        behavior="padding" 
-        enabled 
-        resetScrollToCoords={{ x: 0, y: 0 }}
-        scrollEnabled={false}
-        style={{
-        flex: 1,
-        paddingTop: Constants.statusBarHeight,
-      }}>
-        <Container style={{ backgroundColor: "white" }}>
-          <Content>
-            <Container>
-              <Container style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: 40,
-              }}>
-                <Thumbnail square large source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Eve_Channel_Logo.PNG/250px-Eve_Channel_Logo.PNG' }} />
-              </Container>
-              <Container style={{
-                paddingHorizontal: 30,
-              }}>
-                <Form>
-                <Item floatingLabel>
-                    <Label>Name</Label>
-                    <Input />
-                  </Item>
-                  <Item floatingLabel>
-                    <Label>Email</Label>
-                    <Input />
-                  </Item>
-                  <Item floatingLabel last>
-                    <Label>Password</Label>
-                    <Input />
-                  </Item>
-                </Form>
-                <Container style={{
-                  paddingHorizontal: 45
+    submitForm = async () => {
+        console.log('masok pak ekooooooo')
+        try {
+
+            const { name, email, password } = this.state
+            const { status: existingStatus } = await Permissions.getAsync(
+                Permissions.NOTIFICATIONS
+            )
+            let finalStatus = existingStatus
+
+            if (existingStatus !== 'granted') {
+                const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+                finalStatus = status
+            }
+
+            if (finalStatus !== 'granted') {
+                return
+            }
+
+            let token = await Notifications.getExpoPushTokenAsync()
+            console.log('masok 2')
+            await axios.post('/users/register', {
+                name,
+                email,
+                password,
+                expoNotificationToken: token
+            })
+            console.log('register success')
+            this.props.navigation.navigate('Login')
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView
+                behavior="padding"
+                enabled
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                scrollEnabled={false}
+                style={{
+                    flex: 1,
+                    paddingTop: Constants.statusBarHeight,
                 }}>
-                  <Button  block success rounded style={{
-                    marginTop: 40,
-                  }}>
-                    <Text>submit</Text>
-                  </Button>
-                  <Button block transparent style={{
-                    marginTop: 15
-                  }}>
-                    <Text>login</Text>
-                  </Button>
+                <Container style={{ backgroundColor: "white" }}>
+                    <Content>
+                        <Container>
+                            <Container style={{
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                height: 40,
+                            }}>
+                                <Thumbnail square large source={{ uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Eve_Channel_Logo.PNG/250px-Eve_Channel_Logo.PNG' }} />
+                            </Container>
+                            <Container style={{
+                                paddingHorizontal: 30,
+                            }}>
+                                <Form>
+                                    <Item floatingLabel>
+                                        <Label>Name</Label>
+                                        <Input
+                                            onChangeText={(text) => {
+                                                this.setState({
+                                                    name: text
+                                                })
+                                            }} />
+                                    </Item>
+                                    <Item floatingLabel>
+                                        <Label>Email</Label>
+                                        <Input
+                                            onChangeText={(text) => {
+                                                this.setState({
+                                                    email: text
+                                                })
+                                            }}
+                                        />
+                                    </Item>
+                                    <Item floatingLabel last>
+                                        <Label>Password</Label>
+                                        <Input
+                                            onChangeText={(text) => {
+                                                this.setState({
+                                                    password: text
+                                                })
+                                            }}
+                                        />
+                                    </Item>
+                                </Form>
+                                <Container style={{
+                                    paddingHorizontal: 45
+                                }}>
+                                    <Button
+                                        block
+                                        success
+                                        rounded
+                                        style={{
+                                            marginTop: 40,
+                                        }}
+                                        onPress={() => { this.submitForm() }}
+                                    >
+                                        <Text>submit</Text>
+                                    </Button>
+                                    <Button
+                                        block
+                                        transparent
+                                        style={{
+                                            marginTop: 15
+                                        }}
+                                        onPress={() => this.props.navigation.navigate('Login')}
+                                    >
+                                        <Text>login</Text>
+                                    </Button>
+                                </Container>
+                            </Container>
+                        </Container>
+                    </Content>
                 </Container>
-              </Container>
-            </Container>
-          </Content>
-        </Container>
-      </KeyboardAvoidingView>
-    )
-  }
+            </KeyboardAvoidingView>
+        )
+    }
 }
